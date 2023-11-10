2660412d439837333b34fb9456971a12078d3ad8
Kseniia Murasheva <40736067+Kseymur@users.noreply.github.com>
2023-11-10 19:20:01 +0400

    modified article_checker.py

--- tools/article_checker.py
+++ tools/article_checker.py
@@ -1,69 +1,58 @@
 #!/bin/env python
 
 
-import argparse, os
+import argparse
+import json
+import os
+import sys
 from github import Github
 from tools.git import get_pull_request, get_diff_by_url, parse_diff
 from tools.utils import logging_decorator
 import requests
-import json
 import tiktoken
+import tools.config as config
 
 
-# parse arguments
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    "--github-token", dest="github_token", help="GitHub token", required=True
-)
-parser.add_argument(
-    "--wp-key", dest="wp_key", help="WebPilot API key", required=True
-)
-parser.add_argument(
-    "--pull-url", dest="pull_url", help="GitHub pull URL", required=True
-)
-
-args = parser.parse_args()
-
-wp_key = args.wp_key
-endpoint = 'https://preview.webpilotai.com/api/v1/watt'
-headers = {
-        'Content-Type': 'application/json',
-        'Authorization': f'Bearer {wp_key}'
-    }
+def parse_cli_args():
+    """
+    Parse CLI arguments.
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--github-token", dest="github_token", help="GitHub token", required=True
+    )
+    parser.add_argument(
+        "--api-key", dest="API_key", help="API key", required=True
+    )
+    parser.add_argument(
+        "--pull-url", dest="pull_url", help="GitHub pull URL", required=True
+    )
+    return parser.parse_args()
 
 
-def count_tokens(text):
-    encoding = tiktoken.encoding_for_model("gpt-3.5-turbo")
-    return len(encoding.encode(text))
+def count_tokens(text, model_name="gpt-3.5-turbo"):
+    tokenizer = tiktoken.encoding_for_model(model_name)
+    return len(tokenizer.encode(text))
 
 
-def trimming_prompt(trimmed_prompt):
-    token_count = count_tokens(trimmed_prompt)
-    while token_count > 4096:
-        sentences = trimmed_prompt.rsplit('.', maxsplit=1)
-        if len(sentences) == 2 and sentences[0]:
-            trimmed_prompt = sentences[0].strip() + '.'
-        else:
-            trimmed_prompt = trimmed_prompt.rsplit(' ', maxsplit=1)[0]
-
+def trimming_prompt(prompt, max_tokens):
+    token_count = count_tokens(prompt)
+    trimmed_prompt = prompt
+    while token_count > max_tokens:
+        tokens = trimmed_prompt.split()
+        tokens = tokens[:-1]
+        trimmed_prompt = ' '.join(tokens)
         token_count = count_tokens(trimmed_prompt)
 
     return trimmed_prompt
 
 
-def is_json(myjson):
-    try:
-        json_object = json.loads(myjson)
-    except ValueError as e:
-        return False
-    return True
-
-
 def extract_json_content(content):
-    if is_json(content):
+    try:
         return json.loads(content)
-    else:
-        raise ValueError("Content is not a valid JSON-like string")
+    except json.JSONDecodeError as e:
+        print(f"JSON decoding error: {e}")
+        return None
 
 
 def get_content(diff):
@@ -71,7 +60,6 @@ def get_content(diff):
     Collecting content from Github PR diff.
     Returns the parsed raw text
     """
-
     content = ''
 
     for file in diff:
@@ -89,7 +77,7 @@ def get_content(diff):
     return content
 
 
-def wp_call(PROMPT, headers=headers, endpoint=endpoint):
+def api_call(PROMPT, headers, endpoint):
     """
     Create an API call
     """
@@ -97,11 +85,10 @@ def wp_call(PROMPT, headers=headers, endpoint=endpoint):
     try:
         response = requests.post(endpoint, headers=headers, json=data)
         response.raise_for_status()  # will raise an HTTPError if the HTTP request returned an unsuccessful status code
-        answer = response.text
+        return response.json()
     except requests.exceptions.RequestException as e:
         print(f"Error making API call: {e}")
-        answer = None
-    return answer
+    return None
 
 
 PROMPT = """
@@ -133,7 +120,7 @@ Text for Verification: ```%s```
 
 
 @logging_decorator("Comment on PR")
-def create_comment(pull_request, answer):
+def create_beautiful_comment(pull_request, answer):
     """
     Create a comment on a Github PR.
     """
@@ -151,7 +138,27 @@ def create_comment(pull_request, answer):
         print(f"Error creating a comment on PR: {e}")
 
 
+def create_ugly_comment(pull_request, answer):
+    """
+    Create a comment on a Github PR.
+    """
+    comment = "Here is a result of fact-checking:\n\n"
+    comment += answer
+    print(comment)
+    # only post comment if running on Github Actions
+    if os.environ.get("GITHUB_ACTIONS") == "true":
+        pull_request.create_issue_comment(comment)
+
+
 def main():
+    args = parse_cli_args()
+    endpoint = config.endpoint
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {args.API_key}'
+    }
+    max_tokens = config.max_tokens
+
     github = Github(args.github_token)
     pr = get_pull_request(github, args.pull_url)
     _diff = get_diff_by_url(pr)
@@ -159,21 +166,19 @@ def main():
     content = get_content(diff)
 
     whole_prompt = PROMPT % content
-    print(count_tokens(whole_prompt))
 
-    if count_tokens(whole_prompt) > 4096:
+    if count_tokens(whole_prompt) > max_tokens:
         print("The token limit has been exceeded. Prompt will be trimmed")
-        whole_prompt = trimming_prompt(whole_prompt)
+        whole_prompt = trimming_prompt(whole_prompt, max_tokens)
 
-    claims = wp_call(whole_prompt)
-    claims = json.loads(claims)
-    print(claims)
-    try:
-        # Attempt to extract JSON content
+    claims = api_call(whole_prompt, headers, endpoint)
+    if claims:
         new_claims = extract_json_content(claims['content'])
-        create_comment(pr, new_claims)
-    except ValueError as e:
-        # Handle the case where the content is not JSON-like
-        print(f"Error: {e}")
-        # You can raise the exception again if you want to stop the script here
-        raise
+        if new_claims:
+            create_beautiful_comment(pr, new_claims)
+            print("Created beautiful comment")
+        else:
+            create_ugly_comment(pr, claims)
+            print("Created beautiful comment")
+    else:
+        sys.exit(1)
