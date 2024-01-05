ce327c60dc31bb00e4816b25ae2cf04d3cea03ac
Kseniia Murasheva <40736067+Kseymur@users.noreply.github.com>
2024-01-05 20:27:07 +0400

    fix retriever (#328)
    * fix retriever
    
    * changes for the review task

--- tools/claude_retriever/client.py
+++ tools/claude_retriever/client.py
@@ -4,26 +4,82 @@ from .searcher.types import SearchTool, SearchResult, Tool
 import logging
 import re
 from utils import format_results_full
+import json
 
 logger = logging.getLogger(__name__)
 
+EXTRACTING_PROMPT = """
+Please extract important statements that appear to be factual from the text provided between <text></text> tags.
+Return the extracted statements. Place each statement within <statement></statement> tags.
+Also, return the number of extracted statements within <number_of_statements></number_of_statements> tags.
+Aim to extract important statements with numbers, dates, and names of organizations. There should not be too many extracted statements.
+Skip the preamble; go straight into the result.
+"""
+
 RETRIEVAL_PROMPT = """
-You will be given a query by a human user. Your job is solely to gather information from an external knowledge base that would help the user answer the query. To gather this information, you have been equipped with a search engine tool that you can use to query the external knowledge base. Here is a description of the search engine tool: <tool_description>{description}</tool_description>
+You are tasked with verifying the accuracy of a series of factual statements using a search engine. Below is the search engine's description: <tool_description>{description}</tool_description>.
 
-You can make a call to the search engine tool by inserting a query within <search_query> tags like so: <search_query>query</search_query>. You'll then get results back within <search_result></search_result> tags. After these results back within, reflect briefly inside <search_quality></search_quality> tags about whether all the results together provide enough information to help the user answer the query, or whether more information is needed.
+For each statement within <statement></statement> tags formulate a query to check its accuracy. You can make a call to the search engine tool by inserting a query within <search_query> tags like so: <search_query>query</search_query>. You'll then get results back within <search_result></search_result> tags.
+Results will be provided in <search_result></search_result> tags. Based on these results, determine the accuracy of each statement and categorize it as 'True', 'False', or 'Unverified'.
+Include the Web Page URL in <source></source> tags (use 'None' if no URL is available).
+If a statement is false, include an explanation in <explanation></explanation> tags.
+Focus particularly on verifying numbers, dates, monetary values, and names of people or organizations.
+Avoid verifying statements already enclosed in <search_query>query</search_query> tags.
+Determine the accuracy of each statement using only information that is contained in the search_result. 
 
-Before beginning to research the query, first think for a moment inside <thinking></thinking> tags about what information is necessary to gather to create a well-informed answer. 
-
-If the query is complex, you may need to decompose the query into multiple subqueries and execute them individually. Sometimes the search engine will return empty search results, or the search results may not contain the information you need. In such cases, feel free to search again with a different query.
-
-Do not try to answer the query. Your only job is to gather relevant search results that will help the user answer the query.
-
-Here is the query: <query>{query}</query> 
+Statements to be verified: 
 """
 
 ANSWER_PROMPT = """
-<search_results>{results}</search_results> Using the search results provided within the <search_results></search_results> tags, please answer the following query <query>{query}</query>.
+
+<fact_checking_results>%s</fact_checking_results>
+
+<text>%s</text>
+
+You are an editor. Perform the following tasks:
+1. Using the information provided within the <fact_checking_results></fact_checking_results> tags, 
+please form the desired output with results of fact-checking. There should be required fields "statement", "source", "result". If the result is False, provide an explanation why. If there is no source, put "None" in the "source" field.
+Output example:
+{"fact_checking":
+   [
+   {"statement": "In July 2011, BTC-e experienced a security breach.",
+   "source": "https://bitcoinmagazine.com/business/btc-e-attacked-1343738085",
+   "result": "False",
+   "explanation": "BTC-e experienced a security breach in July 2012, not 2011"
+   }
+   ]}
+2. Make detailed editor's notes on the text in <text></text> tags. 
+Suggest stylistic and grammatical improvements for the text, and point out any error. 
+Put your detailed notes and the list of errors in the field "corrections". 
+The value of this field should be a string.
+
+3. Additionally, since the text between <text></text> is a Markdown document for Hugo SSG, ensure it adheres to specific formatting requirements.
+Output example:
+{"hugo_checking": "False"}
+
+4. Check if the text between <text></text> follows the Markdown format, including appropriate headers.
+Confirm if it meets submission guidelines, particularly the file naming convention ("YYYY-MM-DD-entity-that-was-hacked.md"). Extract the name of the file from the text between <text></text> tags and compare it to the correct name.
+Verify that the text between <text></text> includes only the allowed headers: "## Summary", "## Attackers", "## Losses", "## Timeline", "## Security Failure Causes".
+Check for the presence of specific metadata headers between "---" lines, such as "date", "target-entities", "entity-types", "attack-types", "title", "loss". The text between <text></text> must contain all and only allowed metadata headers.
+Present your findings in a structured JSON format. 
+Output example:
+{"submission_guidelines": {
+       "article_filename": "bla-bla.md",
+       "correct_filename": "2012-07-16-BTC-e.md",
+       "is_filename_correct": "False",
+       "allowed_headers": ["## Summary", "## Attackers", "## Losses", "## Timeline", "## Security Failure Causes"],   
+       "headers_from_text": "None",   
+       "has_allowed_headers": "False",
+       "allowed_metadata_headers": ["date", "target-entities", "entity-types", "attack-types", "title", "loss"],
+       "metadata_headers_from_text": "None",
+       "has_allowed_metadata_headers": "False"
+       }
+    }
+
+Combine the results of all steps into a single JSON and return it to me in <json></json> tags. 
+All quotes in string values must be properly escaped for use with the json.load function in Python. Strictly adhere to the key names.
 """
+
 
 class ClientWithRetrieval(Anthropic):
 
@@ -40,6 +96,13 @@ class ClientWithRetrieval(Anthropic):
         self.search_tool = search_tool
         self.verbose = verbose
     
+    def extract_statements(self, text: str, model: str, temperature: float = 0.0, max_tokens_to_sample: int = 1000):
+        prompt = f"{EXTRACTING_PROMPT} {HUMAN_PROMPT} <text>{text}</text>{AI_PROMPT}"
+        completion = self.completions.create(prompt=prompt, model=model, temperature=temperature, max_tokens_to_sample=max_tokens_to_sample).completion
+            
+        return completion
+    
+
     def retrieve(self,
                        query: str,
                        model: str,
@@ -47,29 +110,35 @@ class ClientWithRetrieval(Anthropic):
                        stop_sequences: list[str] = [HUMAN_PROMPT],
                        max_tokens_to_sample: int = 1000,
                        max_searches_to_try: int = 5,
-                       temperature: float = 1.0) -> list[SearchResult]:
+                       temperature: float = 0.0) -> str:
         """
         Main method to retrieve relevant search results for a query with a provided search tool.
         
         Constructs RETRIEVAL prompt with query and search tool description. 
         Keeps sampling Claude completions until stop sequence hit.
-        Extracts search results and accumulates all raw results.
         
-        Returns:
-            list[SearchResult]: List of all raw search results
+        Returns string with fact-checking results
         """
         assert self.search_tool is not None, "SearchTool must be provided to use .retrieve()"
 
         description = self.search_tool.tool_description
-        prompt = f"{HUMAN_PROMPT} {RETRIEVAL_PROMPT.format(query=query, description=description)}{AI_PROMPT}"
+        statements = self.extract_statements(query, model=model, max_tokens_to_sample=max_tokens_to_sample, temperature=temperature)
+        print("Statements:", statements)
+        num_of_statements = int(self.extract_between_tags("number_of_statements", statements, strip=True))
+        print("num_of_statements:", num_of_statements)
+        prompt = f"{RETRIEVAL_PROMPT.format(description=description)}{HUMAN_PROMPT} {statements} {AI_PROMPT}"
+        print("Prompt:", prompt)
         token_budget = max_tokens_to_sample
         all_raw_search_results: list[SearchResult] = []
-        for tries in range(max_searches_to_try):
+        completions = ""
+        for tries in range(num_of_statements):
             partial_completion = self.completions.create(prompt = prompt,
                                                      stop_sequences=stop_sequences + ['</search_query>'],
                                                      model=model,
                                                      max_tokens_to_sample = token_budget,
                                                      temperature = temperature)
+            print("Partial completion:", partial_completion.completion)
+            completions += partial_completion.completion
             partial_completion, stop_reason, stop_seq = partial_completion.completion, partial_completion.stop_reason, partial_completion.stop # type: ignore
             logger.info(partial_completion)
             token_budget -= self.count_tokens(partial_completion)
@@ -77,39 +146,44 @@ class ClientWithRetrieval(Anthropic):
             if stop_reason == 'stop_sequence' and stop_seq == '</search_query>':
                 logger.info(f'Attempting search number {tries}.')
                 raw_search_results, formatted_search_results = self._search_query_stop(partial_completion, n_search_results_to_use)
+                print("formatted_search_results", formatted_search_results)
                 prompt += '</search_query>' + formatted_search_results
+                completions += '</search_query>' + formatted_search_results
                 all_raw_search_results += raw_search_results
             else:
                 break
-        return all_raw_search_results
-    
-    def answer_with_results(self, raw_search_results: list[str]|list[SearchResult], query: str, model: str, temperature: float, format_results: bool =False):
+        print("all_completions:", completions)
+        return completions
+
+
+    def answer_with_results(self, search_results: str, query: str, model: str, temperature: float):
         """Generates an RAG response based on search results and a query. If format_results is True,
            formats the raw search results first. Set format_results to True if you are using this method standalone without retrieve().
-        
+
         Returns:
             str: Claude's answer to the query
         """
-        if isinstance(raw_search_results[0], str):
-            search_results  = [SearchResult(content=s) for s in raw_search_results] # type: ignore
-
-        if format_results:
-            processed_search_results = [search_result.content.strip() for search_result in search_results] # type: ignore
-            formatted_search_results = format_results_full(processed_search_results)
-        else:
-            formatted_search_results = raw_search_results
         
-        prompt = f"{HUMAN_PROMPT} {ANSWER_PROMPT.format(query=query, results=formatted_search_results)}{AI_PROMPT}"
+        try:
+            prompt = f"{HUMAN_PROMPT} {ANSWER_PROMPT%(search_results, query)} {AI_PROMPT}"
+        except Exception as e:
+            print(str(e))
         
-        answer = self.completions.create(
-            prompt=prompt, 
-            model=model, 
-            temperature=temperature, 
-            max_tokens_to_sample=1000
-        ).completion
+        print("Prompt:", prompt)
+        
+        try:
+            answer = self.completions.create(
+                prompt=prompt, 
+                model=model, 
+                temperature=temperature, 
+                max_tokens_to_sample=4000
+            ).completion
+        except Exception as e:
+            answer = str(e)
         
         return answer
     
+
     def completion_with_retrieval(self,
                                         query: str,
                                         model: str,
@@ -117,7 +191,7 @@ class ClientWithRetrieval(Anthropic):
                                         stop_sequences: list[str] = [HUMAN_PROMPT],
                                         max_tokens_to_sample: int = 1000,
                                         max_searches_to_try: int = 5,
-                                        temperature: float = 1.0) -> str:
+                                        temperature: float = 0.0) -> str:
         """
         Gets a final completion from retrieval results        
         
@@ -132,9 +206,13 @@ class ClientWithRetrieval(Anthropic):
                                                  max_tokens_to_sample=max_tokens_to_sample,
                                                  max_searches_to_try=max_searches_to_try,
                                                  temperature=temperature)
+        print("Search results:", search_results)
         answer = self.answer_with_results(search_results, query, model, temperature)
-        return answer
+        print("Answer:", answer)
+        json_answer = self.extract_between_tags("answer", answer)
+        return json_answer
     
+
     # Helper methods
     def _search_query_stop(self, partial_completion: str, n_search_results_to_use: int) -> Tuple[list[SearchResult], str]:
         """
@@ -165,6 +243,7 @@ class ClientWithRetrieval(Anthropic):
             logger.info('\n' + '-'*20 + f'\nThe SearchTool has returned the following search results:\n\n{formatted_search_results}\n\n' + '-'*20 + '\n')
         return search_results, formatted_search_results
     
+
     def extract_between_tags(self, tag, string, strip=True):
         """
         Helper to extract text between XML tags.
