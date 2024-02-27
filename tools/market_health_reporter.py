8b2b1f0888ed27c488a7be3b7715a398a0b0c2ac
Kseniia Murasheva <40736067+Kseymur@users.noreply.github.com>
2024-02-27 13:54:03 +0000

    fix details in mh-reporter

--- tools/market_health_reporter.py
+++ tools/market_health_reporter.py
@@ -1,4 +1,4 @@
-from openai import OpenAI
+import openai
 from tiktoken import encoding_for_model
 import argparse
 import json
@@ -6,16 +6,15 @@ import os
 import requests
 import glob
 from github import Github
-from tools.claude_retriever.client import extract_between_tags
-from tools.utils import read_file
+from tools.utils import read_file, extract_between_tags
 
 
-REPO_NAME = "1712n/dn-institute"
+REPO_NAME = "1712n/dni-llm-experiments" #"1712n/dn-institute"
 SYSTEM_PROMPT_FILE = 'tools/market_health_reporter_doc/prompts/system_prompt.txt'
 HUMAN_PROMPT_FILE = 'tools/market_health_reporter_doc/prompts/prompt1.txt'
 ARTICLE_EXAMPLE_FILE = 'content/market-health/posts/2023-08-14-huobi/index.md'
-OUTPUT_DIR = 'content/market-health/posts'
-DATA_DIR = 'tools/market_health_reporter_doc/data'
+OUTPUT_DIR = 'content/market-health/posts/'
+DATA_DIR = 'tools/market_health_reporter_doc/data/'
 MAX_TOKENS = 125000
 
 
@@ -57,11 +56,11 @@ def save_output(output: str, directory: str, marketvenueid: str, pairid: str, st
     """
     Saves the output to a markdown file in the specified directory, creating a subdirectory for it.
     """
-    output_subdir = os.path.join(directory, marketvenueid, pairid)  
+    output_subdir = os.path.join(directory, f"{start}-{end}-{marketvenueid}-{pairid}")  
     os.makedirs(output_subdir, exist_ok=True)  
     safe_start = start.replace(":", "-")
     safe_end = end.replace(":", "-")
-    base_file_name = f"{marketvenueid}_{pairid}_{safe_start}_{safe_end}"
+    base_file_name = "index"
     file_path = os.path.join(output_subdir, base_file_name)  
     
     existing_files = glob.glob(f"{file_path}*.md")
@@ -121,9 +120,9 @@ def post_comment_to_issue(github_token, issue_number, repo_name, comment):
     g = Github(github_token)
     repo = g.get_repo(repo_name)
     issue = repo.get_issue(number=issue_number)
-        # only post comment if running on Github Actions
-    if os.environ.get("GITHUB_ACTIONS") == "true":
-        issue.create_comment(comment)
+    # only post comment if running on Github Actions
+    #if os.environ.get("GITHUB_ACTIONS") == "true":
+    issue.create_comment(comment)
 
 
 def create_prompt(article_example: str, data: dict, human_prompt_content: str) -> str:
@@ -141,6 +140,7 @@ def main():
     article_example = read_file(ARTICLE_EXAMPLE_FILE)
 
     marketvenueid, pairid, start, end = extract_data_from_comment(args.comment_body)
+    print(f"Marketvenueid: {marketvenueid}, Pairid: {pairid}, Start: {start}, End: {end}")
     querystring = {
         "marketvenueid": marketvenueid,
         "pairid": pairid,
@@ -167,8 +167,8 @@ def main():
             print(error_message)
             post_comment_to_issue(args.github_token, int(args.issue), REPO_NAME, error_message)
         else:
-            client = OpenAI(api_key=args.API_key)
-            completion = client.chat.completions.create(
+            openai.api_key = args.API_key
+            completion = openai.ChatCompletion.create(
                 model="gpt-4-0125-preview",
                 temperature=0.0,
                 messages=[
@@ -185,3 +185,6 @@ def main():
 
     except Exception as e:
         print(f"Error occurred: {e}")
+
+if __name__ == "__main__":
+    main()
