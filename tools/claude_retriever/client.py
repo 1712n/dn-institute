25d3388475c6f41f5448e304053c47135ddd6aa5
Kseniia Murasheva <40736067+Kseymur@users.noreply.github.com>
2024-04-15 11:57:47 +0000

    fix: refactor client.py

--- tools/claude_retriever/client.py
+++ tools/claude_retriever/client.py
@@ -10,8 +10,7 @@ from datetime import datetime
 
 logger = logging.getLogger(__name__)
 
-HUMAN_PROMPT = "HUMAN_PROMPT"
-AI_PROMPT = "AI_PROMPT"
+
 EXTRACTING_PROMPT = """
 Please extract important statements that appear to be factual from the text provided between <text></text> tags.
 Return the extracted statements. Place each statement within <statement></statement> tags.
@@ -121,7 +120,6 @@ class ClientWithRetrieval:
         Parameters:
             search_tool (SearchTool): SearchTool object to handle searching
             verbose (bool): Whether to print verbose logging
-            *args, **kwargs: Passed to superclass init
         """
         self.search_tool = search_tool
         self.verbose = verbose
@@ -149,7 +147,7 @@ class ClientWithRetrieval:
         return message.content[0].text
     
 
-    def retrieve(self, query: str, model: str, n_search_results_to_use: int = 3, stop_sequences: list[str] = [HUMAN_PROMPT], max_tokens: int = 1000, max_searches_to_try: int = 5, temperature: float = 0.0) -> str:
+    def retrieve(self, query: str, model: str, n_search_results_to_use: int = 3, stop_sequences: list[str] = [], max_tokens: int = 1000, max_searches_to_try: int = 5, temperature: float = 0.0) -> str:
         """
         Main method to retrieve relevant search results for a query with a provided search tool.
 
@@ -163,22 +161,8 @@ class ClientWithRetrieval:
         description = self.search_tool.tool_description
         statements = self.extract_statements(query, model=model, max_tokens=max_tokens, temperature=temperature)
         num_of_statements = int(self.extract_between_tags("number_of_statements", statements, strip=True))
-        print(f'this is num_of_statements: {num_of_statements}')
         current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         system_prompt = f"{RETRIEVAL_PROMPT.format(current_time=current_time, description=description)}"
-        print(f'this is system_prompt: {system_prompt}')
-
-        messages = [
-            {
-                "role": "user",
-                "content": [
-                    {
-                        "type": "text",
-                        "text": statements
-                    }
-                ]
-            }
-        ]
 
         completions = ""
         for tries in range(min(num_of_statements, max_searches_to_try)):
@@ -187,53 +171,56 @@ class ClientWithRetrieval:
                 max_tokens=max_tokens,
                 temperature=temperature,
                 system=system_prompt,
-                messages=messages,
+                messages = [
+                    {
+                        "role": "user",
+                        "content": [
+                            {
+                                "type": "text",
+                                "text": statements
+                            }
+                        ]
+                    }
+                ],
                 stop_sequences=["</search_query>"]
             )
             partial_completion, stop_reason, stop_seq = message.content[0].text, message.stop_reason, message.stop_sequence
-            logger.info(partial_completion)
-            completion = message.content[0].text
-            print(f'this is completion: {completion}')
-            completions += completion
-            messages[0]['content'][0]['text'] += completion
+            print(f'this is the completion: {partial_completion}')
+            completions += partial_completion
+            messages[0]['content'][0]['text'] += partial_completion
             if stop_reason == 'stop_sequence' and stop_seq == '</search_query>':
                 logger.info(f'Attempting search number {tries}.')
                 formatted_search_results = self._search_query_stop(partial_completion, n_search_results_to_use)
-                messages[0]['content'][0]['text'] += '</search_query>' + formatted_search_results
                 completions += '</search_query>' + formatted_search_results
+                messages[0]['content'][0]['text'] += '</search_query>' + formatted_search_results
             else:
                 break
-
         return completions
 
 
     def answer_with_results(self, search_results: str, query: str, model: str, temperature: float):
         prompt = f'<fact_checking_results>{search_results}</fact_checking_results> <text>{query}</text>'
-        messages = [
-            {
-                "role": "user",
-                "content": [
-                    {
-                        "type": "text",
-                        "text": prompt
-                    }
-                ]
-            }
-        ]
-
         try:
             message = self.client.messages.create(
                 model=model,
-                max_tokens=4000,
                 temperature=temperature,
                 system=ANSWER_PROMPT,
-                messages=messages
+                messages = [
+                    {
+                        "role": "user",
+                        "content": [
+                            {
+                                "type": "text",
+                                "text": prompt
+                            }
+                        ]       
+                    }
+                ]
             )
             answer = message.content[0].text
         except Exception as e:
             answer = str(e)
             print(f"An error occurred: {answer}")
-
         return answer
     
 
@@ -241,7 +228,7 @@ class ClientWithRetrieval:
                                         query: str,
                                         model: str,
                                         n_search_results_to_use: int = 3,
-                                        stop_sequences: list[str] = [HUMAN_PROMPT],
+                                        stop_sequences: list[str] = [],
                                         max_tokens: int = 1000,
                                         max_searches_to_try: int = 5,
                                         temperature: float = 0.0) -> str:
@@ -264,7 +251,6 @@ class ClientWithRetrieval:
         return answer
     
 
-    # Helper methods
     def _search_query_stop(self, partial_completion: str, n_search_results_to_use: int) -> Tuple[list[SearchResult], str]:
         """
         Helper to handle search query stop case.
