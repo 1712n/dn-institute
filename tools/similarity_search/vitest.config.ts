676c799cd476fb3a22f26ee5b4073ca6e5ed1ebc
Evgeny Dmitriev <56804873+evgenydmitriev@users.noreply.github.com>
2024-05-10 16:41:24 +0300

    Similarity search worker (#429)
    * similarity worker init
    
    * fix: test setup
    
    * test: add bindings
    
    * fix: api key check
    
    ---------
    
    Co-authored-by: jalmonter <116984708+jalmonter@users.noreply.github.com>

---
+++ tools/similarity_search/vitest.config.ts
@@ -0,0 +1,51 @@
+import { defineWorkersConfig } from "@cloudflare/vitest-pool-workers/config"
+
+export default defineWorkersConfig({
+  test: {
+    poolOptions: {
+      workers: {
+        wrangler: {
+          configPath: "./wrangler.toml"
+        },
+        miniflare: {
+          bindings: {
+            API_KEY_TOKEN_CHECK: "test-api-key"
+          },
+          wrappedBindings: {
+            AI: {
+              scriptName: "workers-ai"
+            },
+            VECTORIZE_INDEX: {
+              scriptName: "vectorize-index"
+            }
+          },
+          workers: [
+            {
+              name: "workers-ai",
+              modules: true,
+              script: `export default function() {
+                return {
+                  run: async (model, data) => {
+                    return Promise.resolve({ data: {} });
+                  }
+                };
+              };`
+            },
+            {
+              name: "vectorize-index",
+              modules: true,
+              script: `export default function() {
+                return {
+                  query: async (vectorData, options) => {
+                    const score = 0.5678;
+                    return Promise.resolve({ matches: [{ score }] });
+                  }
+                };
+              };`
+            }
+          ]
+        }
+      }
+    }
+  }
+})
