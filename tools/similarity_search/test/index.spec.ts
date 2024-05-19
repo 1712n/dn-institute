6805cd88d7c979f9e9ec80bb7f2fa16f24fa6248
Roman Kabanov <white.noise.xl@gmail.com>
2024-05-19 23:19:30 +0200

    Similarity search - add tests for single and batch message processing

--- tools/similarity_search/test/index.spec.ts
+++ tools/similarity_search/test/index.spec.ts
@@ -20,3 +20,64 @@ describe("Authentication", () => {
     expect(await response.text()).toBe("Unauthorized")
   })
 })
+
+describe("Single message processing", () => {
+  it("returns single scalar result when single scalar text is given", async () => {
+    const response = await SELF.fetch("https://example.com/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-API-Key": "test-api-key",
+      },
+      body: JSON.stringify({
+        text: "Sample text",
+        namespace: "test-namespace"
+      })
+    })
+    expect(response.status).toBe(200)
+    expect(await response.text()).toEqual('{"similarity_score":0.5678}')
+  })
+})
+
+describe("Batch message processing", () => {
+  it ("limits max inputs", async () => {
+    const response = await SELF.fetch("https://example.com/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-API-Key": "test-api-key",
+      },
+      body: JSON.stringify({
+        text: [
+          "This is a story about an orange cloud",
+          "This is a story about a llama",
+          "This is a story about a hugging emoji",
+          "This is a story about overwhelming courage",
+        ],
+        namespace: "test-namespace"
+      })
+    })
+    expect(response.status).toBe(400)
+    expect(await response.text()).toEqual("Too big input, property `text` can have max 3 items")
+  })
+
+  it ("returns array results when multiple texts are given", async () => {
+    const response = await SELF.fetch("https://example.com/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-API-Key": "test-api-key",
+      },
+      body: JSON.stringify({
+        text: [
+          "This is a story about an orange cloud",
+          "This is a story about a llama",
+          "This is a story about a hugging emoji"
+        ],
+        namespace: "test-namespace"
+      })
+    })
+    expect(response.status).toBe(200)
+    expect(await response.text()).toEqual('{"similarity_score":[0.5678,0.5678,0.5678]}')
+  })
+})
