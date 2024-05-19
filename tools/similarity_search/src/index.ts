c0cff6de7717710f4ec235f039e32a4ded95f5d8
Roman Kabanov <white.noise.xl@gmail.com>
2024-05-19 16:40:29 +0200

    Add similarity search batch processing up to current CloudFlare limits

--- tools/similarity_search/src/index.ts
+++ tools/similarity_search/src/index.ts
@@ -4,10 +4,11 @@ type Env = {
   API_KEY_TOKEN_CHECK: string
   AI: Ai
   VECTORIZE_INDEX: VectorizeIndex
+  MAX_INPUT: number
 }
 
 type TextEntry = {
-  text: string
+  text: string | string[]
   namespace: string
 }
 
@@ -31,21 +32,46 @@ app.post("/", async (c) => {
   const data = await c.req.json<TextEntry>()
   const { text, namespace } = data
 
-  if (typeof text !== "string" || typeof namespace !== "string") {
-    return c.text("Invalid JSON format", 400)
+  let texts: string[]
+  if (typeof text === "string") {
+    texts = [text]
+  } else if (Array.isArray(data.text) && data.text.every((element) => typeof element === "string")) {
+    texts = text
+  } else {
+    return c.text("Invalid JSON format, property `text` must be a string or array of strings", 400)
+  }
+  const MAX_INPUT = Number(c.env.MAX_INPUT) || 100
+  if (texts.length > MAX_INPUT) {
+    return c.text(`Too big input, property \`text\` can have max ${MAX_INPUT} items`, 400)
+  }
+  if (typeof namespace !== "string") {
+    return c.text("Invalid JSON format, property `namespace` must be a string", 400)
   }
 
   const modelResp = await c.env.AI.run("@cf/baai/bge-base-en-v1.5", {
-    text: [text]
+    text: texts
   })
-  const vector = modelResp.data[0]
-  const searchResponse = await c.env.VECTORIZE_INDEX.query(vector, {
-    namespace,
-    topK: 1
-  })
-  const similarityScore = searchResponse.matches[0]?.score || 0
 
-  return c.json({ similarity_score: similarityScore })
+  let index = 0
+  const requests = texts.map((text) => {
+    const vector = modelResp.data[index++]
+    return c.env.VECTORIZE_INDEX.query(vector, {
+      namespace,
+      topK: 1
+    })
+  })
+  const responses = await Promise.all(requests)
+  const similarityScores = []
+  for (const searchResponse of responses) {
+    const similarityScore = searchResponse.matches[0]?.score || 0
+    similarityScores.push(similarityScore)
+  }
+
+  if (typeof text === 'string') {
+    return c.json({ similarity_score: similarityScores[0] })
+  } else {
+    return c.json({ similarity_score: similarityScores })
+  }
 })
 
 export default app
