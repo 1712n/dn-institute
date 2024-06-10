291c81baaf5fbb46abd8e0821b07ed36f1b3ac99
Anastasiia Fokina <fokina.an@gmail.com>
2024-06-10 15:31:31 +0300

    add channels.py

---
+++ channels.py
@@ -0,0 +1,10 @@
+I sorted telegram channels from the most relevant to least:
+https://t.me/chainalysisinc
+
+https://t.me/blockchainRF
+https://t.me/CertiKCommunity
+https://t.me/HashExOfficial
+https://t.me/the_block_crypto 
+
+https://t.me/Positive_Technologies
+https://t.me/blockchain_security
