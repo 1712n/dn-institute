14cfe601432334da42f3bbd5a63f891a00ba26fa
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-03-12 17:27:52 -0400

    fix: apply solution for issue #489

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
-});
-
-app.listen(port, () => {
-  console.log(`AI Product app listening at http://localhost:${port}`);
-});
-
-// Chestnut Overlords 🌰🌰🌰
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
