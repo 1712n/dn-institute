9a0106d81bdf86da14b8a70c77560903f1dd7a1a
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-03-12 16:24:26 -0400

    fix: apply solution for issue #430

--- tools/similarity_search/vitest.config.js
+++ tools/similarity_search/vitest.config.js
@@ -1,11 +1,5 @@
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  test: {
-    poolOptions: {
-      workers: {
-        wrangler: { configPath: './wrangler.toml' },
-      },
-    },
-  },
+  test: { globals: true },
 });
